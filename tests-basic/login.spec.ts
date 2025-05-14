8b14d94754707f49c9393b27284edc879ce83947
Mahesh Obulareddygari <mahesh.obulareddygari@creatopy.com>
2025-05-14 18:08:11 +0530

    Add Playwright configuration and initial test cases for login and todo app

---
+++ tests-basic/login.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect, type Page } from "@playwright/test";
+
+test.describe("Login Page", () => {
+  let page: Page;
+
+  test.beforeEach(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.goto("/");
+  });
+
+  test.afterEach(async () => {
+    await page.close();
+  });
+
+  test("login with valid credentials", async () => {
+    await page.getByRole("textbox", { name: "Username" }).fill("Admin");
+    await page.getByRole("textbox", { name: "Password" }).fill("admin123");
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(
+      page.locator("span").filter({ hasText: "FirstNameTest LastNameTest" })
+    ).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Dashboard" })
+    ).toBeVisible();
+  });
+
+  test("login with invalid credentials", async () => {
+    await page.getByRole("textbox", { name: "Username" }).fill("InvalidUser");
+    await page
+      .getByRole("textbox", { name: "Password" })
+      .fill("InvalidPassword");
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.locator(".oxd-alert-content")).toHaveText(
+      "Invalid credentials"
+    );
+    await expect(page.getByRole("alert")).toBeVisible();
+    await expect(page.getByRole("alert")).toHaveText("Invalid credentials");
+  });
+});
